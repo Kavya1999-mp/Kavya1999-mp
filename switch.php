d922de2a60b896a81f5f200b3248bb1be190621c
kavya <kavyamp05@gmail.com>
2024-11-29 11:38:31 +0530

    Add files via upload

---
+++ switch.php
@@ -0,0 +1,28 @@
+<html>
+<body>
+<?php
+$choice=2;
+switch($choice)
+{
+case 1:
+     echo"red";
+     break;
+case 2:
+     
+     echo"blue";
+     break;
+case 3:
+     
+     echo"orange";
+     break;
+case 4:
+    
+     echo"green";
+     break;
+default:
+    echo"invalid choice";
+    break;
+}
+?>
+</body>
+</html>
